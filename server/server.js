e8aa8ff156a819a3c06cf3f4fc1c7c8ab00ebc81
Nate Laws <natelaws@gmail.com>
2015-07-06 20:45:16 -0400

    es6 server side

--- server/server.js
+++ server/server.js
@@ -6,21 +6,19 @@ server.connection({port: 3001});
 // Don't do anything when requiring .scss files on the server side
 // Webpack will bundle then into a .css file for the client side
 require.extensions['.scss'] = function (module, filename) {
-    module._compile('', filename);
+    //module._compile('', filename);
 };
 
 
-module.exports.start = function () {
+export function start() {
 
-    server.register(require('./plugins'), function (err) {
+    server.register(require('./plugins'), (err) => {
         if (err) {
             throw err; // something bad happened loading the plugin
         }
 
         server.route(require('./routes'));
 
-        server.start(function () {
-            server.log('info', 'Server running at: ' + server.info.uri);
-        });
+        server.start(() => server.log('info', 'Server running at: ' + server.info.uri));
     });
-};
+}
