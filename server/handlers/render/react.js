d0d9ca8f0ead40f53269fd07fd71052b3c088541
Nate Laws <natelaws@gmail.com>
2015-07-06 20:18:15 -0400

    update babel and replace node-jsx with babel/register for server side compilation

--- server/handlers/render/react.js
+++ server/handlers/render/react.js
@@ -1,5 +1,3 @@
-require('node-jsx').install({extension: '.jsx', harmony: true});
-
 var React = require('react');
 var Router = require('react-router');
 
--- server/index.js
+++ server/index.js
@@ -1,3 +1,3 @@
-
+require("babel/register");
 var server = require('./server');
 server.start();
